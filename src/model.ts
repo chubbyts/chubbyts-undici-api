19e17aee3df24ea157c9d5487a18635bc71079ad
Dominik Zogg <dominik.zogg@hotmail.com>
2022-06-12 20:22:48 +0200

    sort should be asc|desc

--- src/model.ts
+++ src/model.ts
@@ -8,7 +8,7 @@ export type List = {
   offset: number;
   limit: number;
   filters: Record<string, unknown>;
-  sort: Record<string, string>;
   items: Array<Model>;
+  sort: Record<string, 'asc' | 'desc'>;
   count: number;
 };
