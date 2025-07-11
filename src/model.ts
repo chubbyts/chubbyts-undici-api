0ee7f68bd4d4c8d513f2e77bd88db0a9613cead0
Dominik Zogg <dominik.zogg@ikmail.com>
2025-07-11 10:42:34 +0200

    use any/object where needed

--- src/model.ts
+++ src/model.ts
@@ -2,62 +2,65 @@ import type { ServerRequest } from '@chubbyts/chubbyts-http-types/dist/message';
 import { z } from 'zod';
 
 type MakeOptional<S> = S extends z.ZodTypeAny ? z.ZodOptional<S> : never;
+type AddDefault<S extends z.ZodTypeAny> = S | z.ZodDefault<S>;
 
-type ZodSchemaFromType<T> = T extends string
-  ? z.ZodString
-  : T extends number
-    ? z.ZodNumber
-    : T extends boolean
-      ? z.ZodBoolean
-      : T extends bigint
-        ? z.ZodBigInt
-        : T extends symbol
-          ? z.ZodSymbol
-          : T extends Date
-            ? z.ZodDate
-            : T extends undefined
-              ? z.ZodUndefined
-              : T extends null
-                ? z.ZodNull
-                : T extends never
-                  ? z.ZodNever
-                  : T extends Array<infer U>
-                    ? z.ZodArray<ZodSchemaFromType<U>>
-                    : T extends readonly [unknown, ...unknown[]]
-                      ? z.ZodTuple<
-                          Extract<{ [K in keyof T]: ZodSchemaFromType<T[K]> }, [z.ZodTypeAny, ...z.ZodTypeAny[]]>
-                        >
-                      : T extends Map<infer K, infer V>
-                        ? z.ZodMap<ZodSchemaFromType<K>, ZodSchemaFromType<V>>
-                        : T extends Set<infer U>
-                          ? z.ZodSet<ZodSchemaFromType<U>>
-                          : T extends Promise<infer U>
-                            ? z.ZodPromise<ZodSchemaFromType<U>>
-                            : T extends (...args: infer A) => infer R
-                              ? z.ZodFunction<
-                                  z.ZodTuple<
-                                    Extract<
-                                      { [K in keyof A]: ZodSchemaFromType<A[K]> },
-                                      [z.ZodTypeAny, ...z.ZodTypeAny[]]
-                                    >
-                                  >,
-                                  ZodSchemaFromType<R>
-                                >
-                              : T extends object
-                                ? z.ZodObject<
-                                    {
-                                      [K in keyof T]-?: Extract<
-                                        undefined extends T[K]
-                                          ? MakeOptional<ZodSchemaFromType<Exclude<T[K], undefined>>>
-                                          : ZodSchemaFromType<T[K]>,
-                                        z.ZodTypeAny
-                                      >;
-                                    },
-                                    z.UnknownKeysParam,
-                                    z.ZodTypeAny,
-                                    T
+type ZodSchemaFromType<T> = AddDefault<
+  T extends string
+    ? z.ZodString
+    : T extends number
+      ? z.ZodNumber
+      : T extends boolean
+        ? z.ZodBoolean
+        : T extends bigint
+          ? z.ZodBigInt
+          : T extends symbol
+            ? z.ZodSymbol
+            : T extends Date
+              ? z.ZodDate
+              : T extends undefined
+                ? z.ZodUndefined
+                : T extends null
+                  ? z.ZodNull
+                  : T extends never
+                    ? z.ZodNever
+                    : T extends Array<infer U>
+                      ? z.ZodArray<ZodSchemaFromType<U>>
+                      : T extends readonly [unknown, ...unknown[]]
+                        ? z.ZodTuple<
+                            Extract<{ [K in keyof T]: ZodSchemaFromType<T[K]> }, [z.ZodTypeAny, ...z.ZodTypeAny[]]>
+                          >
+                        : T extends Map<infer K, infer V>
+                          ? z.ZodMap<ZodSchemaFromType<K>, ZodSchemaFromType<V>>
+                          : T extends Set<infer U>
+                            ? z.ZodSet<ZodSchemaFromType<U>>
+                            : T extends Promise<infer U>
+                              ? z.ZodPromise<ZodSchemaFromType<U>>
+                              : T extends (...args: infer A) => infer R
+                                ? z.ZodFunction<
+                                    z.ZodTuple<
+                                      Extract<
+                                        { [K in keyof A]: ZodSchemaFromType<A[K]> },
+                                        [z.ZodTypeAny, ...z.ZodTypeAny[]]
+                                      >
+                                    >,
+                                    ZodSchemaFromType<R>
                                   >
-                                : z.ZodAny;
+                                : T extends object
+                                  ? z.ZodObject<
+                                      {
+                                        [K in keyof T]-?: Extract<
+                                          undefined extends T[K]
+                                            ? MakeOptional<ZodSchemaFromType<Exclude<T[K], undefined>>>
+                                            : ZodSchemaFromType<T[K]>,
+                                          z.ZodTypeAny
+                                        >;
+                                      },
+                                      z.UnknownKeysParam,
+                                      z.ZodTypeAny,
+                                      T
+                                    >
+                                  : z.ZodAny
+>;
 
 export const stringSchema = z.string().min(1);
 export const numberSchema = z.coerce.number();
@@ -85,19 +88,20 @@ export type ModelSchema<IM extends InputModel> = ZodSchemaFromType<Model<IM>>;
 
 export type Embedded = {
   _embedded?: {
-    [key: string]: unknown;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
   };
 };
 
 export type Link = {
+  name?: string;
   href: string;
   templated?: boolean;
-  [key: string]: unknown;
 };
 
 export type Links = {
   _links?: {
-    [key: string]: Link;
+    [key: string]: Array<Link> | Link | undefined;
   };
 };
 
@@ -117,8 +121,8 @@ export type Sort = z.infer<typeof sortSchema>;
 export type InputModelList = {
   offset: number;
   limit: number;
-  filters: { [key: string]: unknown };
-  sort: { [key: string]: Sort };
+  filters: object;
+  sort: object;
 };
 
 export type InputModelListSchema = ZodSchemaFromType<InputModelList>;
