3d0834dc0dd92393d5fe63b2cb726dfeaea9ea3d
Dominik Zogg <dominik.zogg@ikmail.com>
2025-08-12 20:37:34 +0200

    api-request-handler

---
+++ src/handler/api.ts
@@ -0,0 +1,201 @@
+import type { ZodTypeAny, z } from 'zod';
+import type { ServerRequest, Response } from '@chubbyts/chubbyts-http-types/dist/message';
+import type { Decoder } from '@chubbyts/chubbyts-decode-encode/dist/decoder';
+import type { Encoder } from '@chubbyts/chubbyts-decode-encode/dist/encoder';
+import type { Handler } from '@chubbyts/chubbyts-http-types/dist/handler';
+import { createBadRequest, createInternalServerError } from '@chubbyts/chubbyts-http-error/dist/http-error';
+import type { StreamFactory } from '@chubbyts/chubbyts-http-types/dist/message-factory';
+import { statusMap } from '@chubbyts/chubbyts-http-types/dist/message';
+import type { Data } from '@chubbyts/chubbyts-decode-encode/dist';
+import { streamToString } from '../stream.js';
+import { zodToInvalidParameters } from '../zod-to-invalid-parameters.js';
+
+type InferOrUndefined<T> = T extends ZodTypeAny ? z.infer<T> : undefined;
+
+export type ApiServerRequest<ReqQuery, ReqAttributes, ReqBody> = Omit<ServerRequest, 'uri' | 'attributes' | 'body'> & {
+  uri: Omit<ServerRequest['uri'], 'query'> & { query: ReqQuery };
+  attributes: ReqAttributes;
+  body: ReqBody;
+};
+
+export type ApiResponse<ResBody> = Omit<Response, 'body'> & {
+  body: ResBody;
+};
+
+export const createApiResponse = <B>(body: B, status: number, reasonPhrase?: string): ApiResponse<B> => {
+  return {
+    status,
+    reasonPhrase: reasonPhrase ?? statusMap.get(status) ?? '',
+    protocolVersion: '1.0',
+    headers: {},
+    body,
+  };
+};
+
+type ApiHandlerSchema = {
+  serverRequest: {
+    uri: { query: ZodTypeAny };
+    attributes: ZodTypeAny;
+    body?: ZodTypeAny;
+  };
+  response: {
+    body?: ZodTypeAny;
+  };
+};
+
+type Normalized<Schema extends ApiHandlerSchema> = {
+  serverRequest: {
+    uri: Schema['serverRequest']['uri'];
+    attributes: Schema['serverRequest']['attributes'];
+    body: 'body' extends keyof Schema['serverRequest'] ? Schema['serverRequest']['body'] : undefined;
+  };
+  response: {
+    body: 'body' extends keyof Schema['response'] ? Schema['response']['body'] : undefined;
+  };
+};
+
+type RequiredAttributes<Schema extends ApiHandlerSchema> =
+  (Normalized<Schema>['serverRequest']['body'] extends undefined
+    ? // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+      {}
+    : { contentType: string }) &
+    (Normalized<Schema>['response']['body'] extends undefined
+      ? // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+        {}
+      : { accept: string });
+
+type ValidateApiHandlerSchema<Schema extends ApiHandlerSchema> =
+  z.infer<Schema['serverRequest']['attributes']> extends RequiredAttributes<Schema> ? Schema : never;
+
+export type CreateApiHandlerAdapter = <Schema extends ApiHandlerSchema>(
+  schema: ValidateApiHandlerSchema<Schema>,
+  apiHandler: (
+    apiServerRequest: ApiServerRequest<
+      z.infer<Schema['serverRequest']['uri']['query']>,
+      z.infer<Schema['serverRequest']['attributes']>,
+      InferOrUndefined<Schema['serverRequest']['body']>
+    >,
+  ) => Promise<ApiResponse<InferOrUndefined<Schema['response']['body']>>>,
+) => Handler & { _apiHandler: typeof apiHandler };
+
+export const createApiHandlerAdapterFactory = (
+  decoder: Decoder,
+  encoder: Encoder,
+  streamFactory: StreamFactory,
+): CreateApiHandlerAdapter => {
+  return <Schema extends ApiHandlerSchema>(
+    schema: Schema,
+    apiHandler: (
+      apiServerRequest: ApiServerRequest<
+        z.infer<Schema['serverRequest']['uri']['query']>,
+        z.infer<Schema['serverRequest']['attributes']>,
+        InferOrUndefined<Schema['serverRequest']['body']>
+      >,
+    ) => Promise<ApiResponse<InferOrUndefined<Schema['response']['body']>>>,
+  ): Handler & { _apiHandler: typeof apiHandler } => {
+    const resolveServerRequestUriQuery = (serverRequest: ServerRequest) => {
+      const serverRequestUriQueryResult = schema.serverRequest.uri.query.safeParse(serverRequest.uri.query);
+      if (!serverRequestUriQueryResult.success) {
+        throw createBadRequest({
+          detail: 'Invalid query',
+          invalidParameters: zodToInvalidParameters(serverRequestUriQueryResult.error),
+        });
+      }
+
+      return serverRequestUriQueryResult.data;
+    };
+
+    const resolveServerRequestAttributes = (serverRequest: ServerRequest) => {
+      const serverRequestAttributesResult = schema.serverRequest.attributes.safeParse(serverRequest.attributes);
+      if (!serverRequestAttributesResult.success) {
+        throw createInternalServerError({
+          detail: 'Invalid attributes',
+          invalidParameters: zodToInvalidParameters(serverRequestAttributesResult.error),
+        });
+      }
+
+      return serverRequestAttributesResult.data;
+    };
+
+    const resolveServerRequestBody = async (
+      serverRequest: ServerRequest,
+      serverRequestAttributes: { contentType?: string },
+    ) => {
+      if (!schema.serverRequest.body || !serverRequestAttributes['contentType']) {
+        return undefined;
+      }
+
+      const serverRequestBodyResult = schema.serverRequest.body.safeParse(
+        decoder.decode(await streamToString(serverRequest.body), serverRequestAttributes.contentType, {
+          request: serverRequest,
+        }),
+      );
+
+      if (!serverRequestBodyResult.success) {
+        throw createBadRequest({
+          detail: 'Invalid body',
+          invalidParameters: zodToInvalidParameters(serverRequestBodyResult.error),
+        });
+      }
+
+      return serverRequestBodyResult.data;
+    };
+
+    const resolveResponseBody = (
+      serverRequest: ServerRequest,
+      serverRequestAttributes: { accept?: string },
+      apiResponse: Schema['response'],
+    ) => {
+      if (!schema.response.body || !serverRequestAttributes['accept']) {
+        return streamFactory('');
+      }
+
+      const apiResponseBodyResult = schema.response.body.safeParse(apiResponse.body);
+
+      if (!apiResponseBodyResult.success) {
+        throw createInternalServerError({
+          detail: 'Invalid body',
+          invalidParameters: zodToInvalidParameters(apiResponseBodyResult.error),
+        });
+      }
+
+      return streamFactory(
+        encoder.encode(apiResponseBodyResult.data as Data, serverRequestAttributes.accept, {
+          request: serverRequest,
+        }),
+      );
+    };
+
+    const handler = async (serverRequest: ServerRequest): Promise<Response> => {
+      const serverRequestUriQuery = resolveServerRequestUriQuery(serverRequest);
+      const serverRequestAttributes = resolveServerRequestAttributes(serverRequest);
+      const serverRequestBody = await resolveServerRequestBody(serverRequest, serverRequestAttributes);
+
+      const apiResponse = await apiHandler({
+        ...serverRequest,
+        uri: {
+          ...serverRequest.uri,
+          query: serverRequestUriQuery,
+        },
+        attributes: serverRequestAttributes,
+        body: serverRequestBody,
+      });
+
+      const responseBody = resolveResponseBody(serverRequest, serverRequestAttributes, apiResponse);
+
+      return {
+        ...apiResponse,
+        headers: {
+          ...apiResponse.headers,
+          'content-type': responseBody ? [serverRequestAttributes.accept] : [],
+        },
+        body: responseBody,
+      };
+    };
+
+    // eslint-disable-next-line functional/immutable-data
+    handler._apiHandler = apiHandler;
+
+    return handler;
+  };
+};
---
+++ tests/handler/api.test.ts
@@ -0,0 +1,317 @@
+import { PassThrough } from 'stream';
+import type { Decoder } from '@chubbyts/chubbyts-decode-encode/dist/decoder';
+import type { Encoder } from '@chubbyts/chubbyts-decode-encode/dist/encoder';
+import type { Response, ServerRequest, Uri } from '@chubbyts/chubbyts-http-types/dist/message';
+import type { StreamFactory } from '@chubbyts/chubbyts-http-types/dist/message-factory';
+import { describe, expect, test } from 'vitest';
+import { z } from 'zod';
+import { useObjectMock } from '@chubbyts/chubbyts-function-mock/dist/object-mock';
+import { useFunctionMock } from '@chubbyts/chubbyts-function-mock/dist/function-mock';
+import { HttpError } from '@chubbyts/chubbyts-http-error/dist/http-error';
+import { createApiHandlerAdapterFactory, createApiResponse } from '../../src/handler/api';
+
+describe('createApiHandlerAdapterFactory', () => {
+  test('not matching query', async () => {
+    const serverRequest: ServerRequest = {
+      protocolVersion: '1.0',
+      method: 'POST',
+      uri: {
+        schema: 'https',
+        userInfo: 'user:password',
+        host: 'localhost',
+        port: 443,
+        path: '/path',
+        query: {},
+        fragment: 'fragment',
+      },
+      attributes: {},
+      headers: {},
+      body: new PassThrough(),
+    };
+
+    const [decoder, decoderMocks] = useObjectMock<Decoder>([]);
+
+    const [encoder, encoderMocks] = useObjectMock<Encoder>([]);
+
+    const [streamFactory, streamFactoryMocks] = useFunctionMock<StreamFactory>([]);
+
+    const apiHandlerAdapterFactory = createApiHandlerAdapterFactory(decoder, encoder, streamFactory);
+
+    const apiHandlerAdapter = apiHandlerAdapterFactory(
+      {
+        serverRequest: {
+          uri: {
+            query: z.object({ serverRequestQueryKey: z.string() }),
+          },
+          attributes: z.object({
+            contentType: z.string(),
+            accept: z.string(),
+            serverRequestAttributeKey: z.string(),
+          }),
+          body: z.object({ serverRequestBodyKey: z.string() }),
+        },
+        response: {
+          body: z.object({ responseBodyKey: z.string() }),
+        },
+      },
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      async () => {},
+    );
+
+    try {
+      await apiHandlerAdapter(serverRequest);
+      throw new Error('expect fail');
+    } catch (e) {
+      expect(e).toBeInstanceOf(HttpError);
+
+      expect({ ...e }).toMatchInlineSnapshot(`
+        {
+          "_httpError": "BadRequest",
+          "detail": "Invalid query",
+          "invalidParameters": [
+            {
+              "context": {
+                "code": "invalid_type",
+                "expected": "string",
+              },
+              "name": "serverRequestQueryKey",
+              "reason": "Invalid input: expected string, received undefined",
+            },
+          ],
+          "status": 400,
+          "title": "Bad Request",
+          "type": "https://datatracker.ietf.org/doc/html/rfc2616#section-10.4.1",
+        }
+      `);
+    }
+
+    expect(decoderMocks.length).toBe(0);
+    expect(encoderMocks.length).toBe(0);
+    expect(streamFactoryMocks.length).toBe(0);
+  });
+
+  test('not matching attributes', async () => {
+    const serverRequest: ServerRequest = {
+      protocolVersion: '1.0',
+      method: 'POST',
+      uri: {
+        schema: 'https',
+        userInfo: 'user:password',
+        host: 'localhost',
+        port: 443,
+        path: '/path',
+        query: {
+          serverRequestQueryKey: 'serverRequestQueryValue',
+        },
+        fragment: 'fragment',
+      },
+      attributes: {},
+      headers: {},
+      body: new PassThrough(),
+    };
+
+    const [decoder, decoderMocks] = useObjectMock<Decoder>([]);
+
+    const [encoder, encoderMocks] = useObjectMock<Encoder>([]);
+
+    const [streamFactory, streamFactoryMocks] = useFunctionMock<StreamFactory>([]);
+
+    const apiHandlerAdapterFactory = createApiHandlerAdapterFactory(decoder, encoder, streamFactory);
+
+    const apiHandlerAdapter = apiHandlerAdapterFactory(
+      {
+        serverRequest: {
+          uri: {
+            query: z.object({ serverRequestQueryKey: z.string() }),
+          },
+          attributes: z.object({
+            contentType: z.string(),
+            accept: z.string(),
+            serverRequestAttributeKey: z.string(),
+          }),
+          body: z.object({ serverRequestBodyKey: z.string() }),
+        },
+        response: {
+          body: z.object({ responseBodyKey: z.string() }),
+        },
+      },
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      async () => {},
+    );
+
+    try {
+      await apiHandlerAdapter(serverRequest);
+      throw new Error('expect fail');
+    } catch (e) {
+      expect(e).toBeInstanceOf(HttpError);
+
+      expect({ ...e }).toMatchInlineSnapshot(`
+        {
+          "_httpError": "InternalServerError",
+          "detail": "Invalid attributes",
+          "invalidParameters": [
+            {
+              "context": {
+                "code": "invalid_type",
+                "expected": "string",
+              },
+              "name": "contentType",
+              "reason": "Invalid input: expected string, received undefined",
+            },
+            {
+              "context": {
+                "code": "invalid_type",
+                "expected": "string",
+              },
+              "name": "accept",
+              "reason": "Invalid input: expected string, received undefined",
+            },
+            {
+              "context": {
+                "code": "invalid_type",
+                "expected": "string",
+              },
+              "name": "serverRequestAttributeKey",
+              "reason": "Invalid input: expected string, received undefined",
+            },
+          ],
+          "status": 500,
+          "title": "Internal Server Error",
+          "type": "https://datatracker.ietf.org/doc/html/rfc2616#section-10.5.1",
+        }
+      `);
+    }
+
+    expect(decoderMocks.length).toBe(0);
+    expect(encoderMocks.length).toBe(0);
+    expect(streamFactoryMocks.length).toBe(0);
+  });
+
+  test('successfully', async () => {
+    const serverRequestUriQuery = {
+      serverRequestQueryKey: 'serverRequestQueryValue',
+    };
+
+    const serverRequestUri: Uri = {
+      schema: 'https',
+      userInfo: 'user:password',
+      host: 'localhost',
+      port: 443,
+      path: '/path',
+      query: serverRequestUriQuery,
+      fragment: 'fragment',
+    };
+
+    const serverRequestAttributes = {
+      contentType: 'application/json',
+      accept: 'application/json',
+      serverRequestAttributeKey: 'serverRequestAttributeValue',
+    };
+
+    const serverRequestHeaders = {
+      'content-type': ['application/json'],
+      accept: ['application/json'],
+    };
+
+    const serverRequestData = { serverRequestBodyKey: 'serverRequestBodyValue' };
+
+    const serverRequestBody = new PassThrough();
+    serverRequestBody.write(JSON.stringify(serverRequestData));
+    serverRequestBody.end();
+
+    const serverRequest: ServerRequest = {
+      protocolVersion: '1.0',
+      method: 'POST',
+      uri: serverRequestUri,
+      attributes: serverRequestAttributes,
+      headers: serverRequestHeaders,
+      body: serverRequestBody,
+    };
+
+    const responseData = { responseBodyKey: 'responseBodyValue' };
+
+    const responseBody = new PassThrough();
+    responseBody.write(JSON.stringify(responseData));
+    responseBody.end();
+
+    const response: Response = {
+      protocolVersion: '1.0',
+      status: 200,
+      reasonPhrase: 'OK',
+      headers: {
+        'content-type': ['application/json'],
+      },
+      body: responseBody,
+    };
+
+    const [decoder, decoderMocks] = useObjectMock<Decoder>([
+      {
+        name: 'decode',
+        parameters: [JSON.stringify(serverRequestData), 'application/json', { request: serverRequest }],
+        return: serverRequestData,
+      },
+    ]);
+
+    const [encoder, encoderMocks] = useObjectMock<Encoder>([
+      {
+        name: 'encode',
+        parameters: [responseData, 'application/json', { request: serverRequest }],
+        return: JSON.stringify(responseData),
+      },
+    ]);
+
+    const [streamFactory, streamFactoryMocks] = useFunctionMock<StreamFactory>([
+      { parameters: [JSON.stringify(responseData)], return: responseBody },
+    ]);
+
+    const apiHandlerAdapterFactory = createApiHandlerAdapterFactory(decoder, encoder, streamFactory);
+
+    const apiHandlerAdapter = apiHandlerAdapterFactory(
+      {
+        serverRequest: {
+          uri: {
+            query: z.object({ serverRequestQueryKey: z.string() }),
+          },
+          attributes: z.object({
+            contentType: z.string(),
+            accept: z.string(),
+            serverRequestAttributeKey: z.string(),
+          }),
+          body: z.object({ serverRequestBodyKey: z.string() }),
+        },
+        response: {
+          body: z.object({ responseBodyKey: z.string() }),
+        },
+      },
+      async (apiServerRequest) => {
+        expect(apiServerRequest.protocolVersion).toBe('1.0');
+        expect(apiServerRequest.method).toBe('POST');
+        expect(apiServerRequest.uri).toStrictEqual(serverRequestUri);
+        expect(apiServerRequest.attributes).toStrictEqual(serverRequestAttributes);
+        expect(apiServerRequest.headers).toStrictEqual(serverRequestHeaders);
+        expect(apiServerRequest.body).toStrictEqual(serverRequestData);
+
+        return createApiResponse({ responseBodyKey: 'responseBodyValue' }, 200);
+      },
+    );
+
+    expect(await apiHandlerAdapter(serverRequest)).toStrictEqual(response);
+
+    expect(
+      await apiHandlerAdapter._apiHandler({
+        protocolVersion: '1.0',
+        method: 'POST',
+        uri: { ...serverRequestUri, query: serverRequestUriQuery },
+        attributes: serverRequestAttributes,
+        headers: serverRequestHeaders,
+        body: serverRequestData,
+      }),
+    ).toStrictEqual({ ...response, headers: {}, body: { responseBodyKey: 'responseBodyValue' } });
+
+    expect(decoderMocks.length).toBe(0);
+    expect(encoderMocks.length).toBe(0);
+    expect(streamFactoryMocks.length).toBe(0);
+  });
+});
